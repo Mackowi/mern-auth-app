e403ee448640d411c21003089b821576d70a3a76
Mackowi <dzierzenga.maciej@gmail.com>
2023-05-26 15:06:23 +0200

    added jwt token generation, cookie creation, register and login functionalities

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,57 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // desc:   auth user -> set token
 // route:  POST /api/users/auth
 // access  public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth User Controller' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // desc:   register new user
 // route:  POST /api/users
 // access  public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register User Controller' });
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // desc:   logout user
